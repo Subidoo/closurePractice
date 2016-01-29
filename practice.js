0fb0175d77a9142dd99b461ee9170b2f11c55b29
Kyle Prestgard <ktprestgard@gmail.com>
2016-01-28 19:56:59 -0700

    closures

--- practice.js
+++ practice.js
@@ -10,11 +10,12 @@ var outer = function(){
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
-
+inner();
 
 
 //Next problem
@@ -33,7 +34,8 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
-
+var calling = callFriend();
+calling('435-215-9248');
 
 
 //Next Problem
@@ -45,11 +47,18 @@ var callFriend = function(){
 */
 
   //Code Here
-  var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  function makeCounter() {
+    var num = 0;
+    return function() {
+      num++;
+      return num;
+      }
+    }
+var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -63,7 +72,13 @@ var callFriend = function(){
   Once completed, add a second argument that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+function accept(first, N) {
+  return function() {
+    
+  }
+}
 
+var isNew = accept();
 
 
 
